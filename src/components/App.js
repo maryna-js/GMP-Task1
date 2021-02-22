9ec10018b17befafb1f36308acdeaa3ae9036ecf
maryna-js <rastargueva.m@gmail.com>
2021-02-22 18:50:31 +0300

    feat: check test working

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from './button/button';
 
 import '../styles/style.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div>
                 <h1>Start Page</h1>
+                <Button label="component to test" />
             </div>
         );
     }
